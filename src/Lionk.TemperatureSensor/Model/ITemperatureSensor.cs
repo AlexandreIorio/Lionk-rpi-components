b7d6f7183a6b6ad5f8050a59f60273c1712ec381
Alexandre Iorio <alexandre.iorio@heig-vd.ch>
2024-08-14 23:39:26 +0200

    Work on views and interfaces

--- src/Lionk.TemperatureSensor/Model/ITemperatureSensor.cs
+++ src/Lionk.TemperatureSensor/Model/ITemperatureSensor.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright © 2024 Lionk Project
 
+using Lionk.Core;
 using Lionk.Core.Component;
 
 namespace Lionk.DS18B20;
@@ -7,6 +8,7 @@ namespace Lionk.DS18B20;
 /// <summary>
 /// This interface is used to get the temperature of sensor connected to a Raspberry Pi.
 /// </summary>
+[NamedElement("Temperature Sensor", "Temperature Sensor")]
 public interface ITemperatureSensor : IMeasurableComponent<double>
 {
     /// <summary>
